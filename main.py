db27e645244f0e51fdb847e4ceccef2fe0dc31dd
Rasha92zee <rashahasoon923@gmail.com>
2024-11-15 16:08:56 +0530

    Collision

--- main.py
+++ main.py
@@ -298,6 +298,7 @@ class Decoration(pygame.sprite.Sprite):
         self.rect.midtop = (x + TILE_SIZE // 2, y + (TILE_SIZE - self.image.get_height()))
 
 
+
 class Water(pygame.sprite.Sprite):
     def __init__(self, img, x, y):
         pygame.sprite.Sprite.__init__(self)
